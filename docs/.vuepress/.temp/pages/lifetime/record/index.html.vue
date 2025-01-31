61099f247effac8e5f2a8db58fcb3c4c02981b48
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-01-31 23:11:04 +0800

    doc:lifetime camera

---
+++ docs/.vuepress/.temp/pages/lifetime/record/index.html.vue
@@ -0,0 +1,7 @@
+<template><div><h2 id="_2019" tabindex="-1"><a class="header-anchor" href="#_2019"><span>2019</span></a></h2>
+<VPCardMasonry><p><img src="/assets/camera_2018_01.jpg" alt=""></p>
+<p><img src="/assets/camera_2018_02.jpg" alt=""></p>
+<p><img src="/assets/camera_2018_03.jpg" alt=""></p>
+</VPCardMasonry></div></template>
+
+
---
+++ docs/notes/lifetime/camera.md
@@ -0,0 +1,18 @@
+---
+title: 随影
+createTime: 2025/01/31 22:13:32
+permalink: /lifetime/record/
+---
+
+## 2019
+
+::: card-masonry
+
+![](/assets/camera_2018_01.jpg)
+
+![](/assets/camera_2018_02.jpg)
+
+![](/assets/camera_2018_03.jpg)
+
+:::
+
