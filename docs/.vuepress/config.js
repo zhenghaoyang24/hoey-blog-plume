7b2a5fd2df4faa7f82a3a83a536072a23eee333a
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-11-30 15:31:57 +0800

    chore: plugin-widget 1.1.14

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -103,7 +103,7 @@ export default defineUserConfig({
         {
           name: "Cruel Summer",
           artist: "Taylor Swift",
-          url: "https://api.zhenghaoyang.cn/api/music/1",
+          url: "https://api.zhenghaoyang.cn/music/get/1",
           cover: "https://zhenghaoyang.cn/avatar.jpg",
         },
       ],
