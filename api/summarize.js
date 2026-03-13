1af3b765426673ed80fe9a6baabc836cae43cce2
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2026-03-13 15:10:05 +0800

    chore(config): update development origins for consistency

--- api/summarize.js
+++ api/summarize.js
@@ -25,10 +25,9 @@ const CONFIG = {
   // 开发环境允许
   DEV_ORIGINS: [
     'http://localhost:3000',
-    'http://localhost:5173',
-    'http://localhost:4173',
+    'http://localhost:8080',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173',
+    'http://127.0.0.1:8080',
   ],
 };
 
